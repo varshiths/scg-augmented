54b2b32813e2a3d3ebcf3501997ec5354243c64e
Varshith S <varshith.lfhs@gmail.com>
2019-03-14 15:43:24 +0530

    add missing lib/ files

---
+++ lib/rel_model_tc.py
@@ -0,0 +1,153 @@
+"""
+Let's get the relationships yo
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torch.nn.utils.rnn import PackedSequence
+from lib.resnet import resnet_l4
+from config import BATCHNORM_MOMENTUM
+from lib.fpn.nms.functions.nms import apply_nms
+
+# from lib.decoder_rnn import DecoderRNN, lstm_factory, LockedDropout
+from lib.lstm.decoder_rnn import DecoderRNN
+from lib.lstm.highway_lstm_cuda.alternating_highway_lstm import AlternatingHighwayLSTM
+from lib.fpn.box_utils import bbox_overlaps, center_size
+from lib.get_union_boxes import UnionBoxesAndFeats
+from lib.fpn.proposal_assignments.rel_assignments import rel_assignments
+from lib.object_detector import ObjectDetector, gather_res, load_vgg
+from lib.pytorch_misc import transpose_packed_sequence_inds, to_onehot, arange, enumerate_by_image, diagonal_inds, Flattener
+from lib.sparse_targets import FrequencyBias
+from lib.surgery import filter_dets
+from lib.word_vectors import obj_edge_vectors
+from lib.fpn.roi_align.functions.roi_align import RoIAlignFunction
+
+from lib.rel_model import RelModel
+from lib.rel_model import _sort_by_score
+from lib.rel_model import MODES
+
+import math
+
+
+class RelModelTC(RelModel):
+    """
+    RELATIONSHIPS WITH Teacher Student Framework
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        :param classes: Object classes
+        :param rel_classes: Relationship classes. None if were not using rel mode
+        :param mode: (sgcls, predcls, or sgdet)
+        :param num_gpus: how many GPUS 2 use
+        :param use_vision: Whether to use vision in the final product
+        :param require_overlap_det: Whether two objects must intersect
+        :param embed_dim: Dimension for all embeddings
+        :param hidden_dim: LSTM hidden size
+        :param obj_dim:
+        """
+        self.prior_weight = kwargs.pop("prior_weight", 1.0)
+        kwargs["use_bias"] = True
+        super(RelModelTC, self).__init__(*args, **kwargs)
+
+
+    def forward(self, x, im_sizes, image_offset,
+                gt_boxes=None, gt_classes=None, gt_rels=None, proposals=None, train_anchor_inds=None,
+                return_fmap=False):
+        """
+        Forward pass for detection
+        :param x: Images@[batch_size, 3, IM_SIZE, IM_SIZE]
+        :param im_sizes: A numpy array of (h, w, scale) for each image.
+        :param image_offset: Offset onto what image we're on for MGPU training (if single GPU this is 0)
+        :param gt_boxes:
+
+        Training parameters:
+        :param gt_boxes: [num_gt, 4] GT boxes over the batch.
+        :param gt_classes: [num_gt, 2] gt boxes where each one is (img_id, class)
+        :param train_anchor_inds: a [num_train, 2] array of indices for the anchors that will
+                                  be used to compute the training loss. Each (img_ind, fpn_idx)
+        :return: If train:
+            scores, boxdeltas, labels, boxes, boxtargets, rpnscores, rpnboxes, rellabels
+            
+            if test:
+            prob dists, boxes, img inds, maxscores, classes
+            
+        """
+        result = self.detector(x, im_sizes, image_offset, gt_boxes, gt_classes, gt_rels, proposals,
+                               train_anchor_inds, return_fmap=True)
+        if result.is_none():
+            return ValueError("heck")
+
+        im_inds = result.im_inds - image_offset
+        boxes = result.rm_box_priors
+
+        if self.training and result.rel_labels is None:
+            assert self.mode == 'sgdet'
+            result.rel_labels = rel_assignments(im_inds.data, boxes.data, result.rm_obj_labels.data,
+                                                gt_boxes.data, gt_classes.data, gt_rels.data,
+                                                image_offset, filter_non_overlap=True,
+                                                num_sample_per_gt=1)
+
+        rel_inds = self.get_rel_inds(result.rel_labels, im_inds, boxes)
+
+        rois = torch.cat((im_inds[:, None].float(), boxes), 1)
+
+        result.obj_fmap = self.obj_feature_map(result.fmap.detach(), rois)
+
+        # Prevent gradients from flowing back into score_fc from elsewhere
+        result.rm_obj_dists, result.obj_preds, edge_ctx = self.context(
+            result.obj_fmap,
+            result.rm_obj_dists.detach(),
+            im_inds, result.rm_obj_labels if self.training or self.mode == 'predcls' else None,
+            boxes.data, result.boxes_all)
+
+        if edge_ctx is None:
+            edge_rep = self.post_emb(result.obj_preds)
+        else:
+            edge_rep = self.post_lstm(edge_ctx)
+
+        # Split into subject and object representations
+        edge_rep = edge_rep.view(edge_rep.size(0), 2, self.pooling_dim)
+
+        subj_rep = edge_rep[:, 0]
+        obj_rep = edge_rep[:, 1]
+
+        prod_rep = subj_rep[rel_inds[:, 1]] * obj_rep[rel_inds[:, 2]]
+
+        if self.use_vision:
+            vr = self.visual_rep(result.fmap.detach(), rois, rel_inds[:, 1:])
+            if self.limit_vision:
+                # exact value TBD
+                prod_rep = torch.cat((prod_rep[:,:2048] * vr[:,:2048], prod_rep[:,2048:]), 1)
+            else:
+                prod_rep = prod_rep * vr
+
+        if self.use_tanh:
+            prod_rep = F.tanh(prod_rep)
+
+        result.rel_dists = self.rel_compress(prod_rep)
+        teacher_rel_dists = result.rel_dists + self.prior_weight * self.freq_bias.index_with_labels(torch.stack((
+            result.obj_preds[rel_inds[:, 1]],
+            result.obj_preds[rel_inds[:, 2]],
+        ), 1))
+        result.teacher_rel_soft_preds = F.softmax(teacher_rel_dists, dim=1)
+
+        if self.training:
+            return result
+
+        twod_inds = arange(result.obj_preds.data) * self.num_classes + result.obj_preds.data
+        result.obj_scores = F.softmax(result.rm_obj_dists, dim=1).view(-1)[twod_inds]
+
+        # Bbox regression
+        if self.mode == 'sgdet':
+            bboxes = result.boxes_all.view(-1, 4)[twod_inds].view(result.boxes_all.size(0), 4)
+        else:
+            # Boxes will get fixed by filter_dets function.
+            bboxes = result.rm_box_priors
+
+        rel_rep = F.softmax(result.rel_dists, dim=1)
+        return filter_dets(bboxes, result.obj_scores,
+                           result.obj_preds, rel_inds[:, 1:], rel_rep)
