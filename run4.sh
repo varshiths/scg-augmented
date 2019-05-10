a85a690bdaf3b4b6bb3063cc71875722c7af0a31
varshiths <varshiths@cse.iitb.ac.in>
2019-05-10 10:30:44 +0530

    remove redundant eval_rels_tc.py

--- run4.sh
+++ run4.sh
@@ -18,7 +18,7 @@ python models/eval_rels.py \
     -ckpt checkpoints/motifnet-size-predcls-nbg-3-nob/vgrel-5.tar \
     -cache cache/sgdet_motifnet-size-predcls-nbg-3-nob_vgrel-5
 
-python models/eval_rels_tc.py \
+python models/eval_rels.py \
     -m predcls \
     -model motifnet \
     -order size \
@@ -29,7 +29,7 @@ python models/eval_rels_tc.py \
     -ckpt checkpoints/motifnet-size-predcls-nbg-3-coco-1.0-0.5/vgrel-12.tar \
     -cache cache/sgdet_motifnet-size-predcls-nbg-3-coco-1.0-0.5_vgrel-12
 
-python models/eval_rels_tc.py \
+python models/eval_rels.py \
     -m predcls \
     -model motifnet \
     -order size \
@@ -40,7 +40,7 @@ python models/eval_rels_tc.py \
     -ckpt checkpoints/motifnet-size-predcls-nbg-3-vg-1.0-0.5/vgrel-10.tar \
     -cache cache/sgdet_motifnet-size-predcls-nbg-3-vg-1.0-0.5_vgrel-10
 
-python models/eval_rels_tc.py \
+python models/eval_rels.py \
     -m predcls \
     -model motifnet \
     -order size \
